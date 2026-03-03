dd9bd9c5cd3b036705bc18ef1c66b88ed6ab96f2
IqbalRizqiR <rama.ramajr121@gmail.com>
2026-03-03 14:41:49 +0700

    oke

--- client-apps/src/app/api/(auth)/forgot-password/route.ts
+++ client-apps/src/app/api/(auth)/forgot-password/route.ts
@@ -8,7 +8,7 @@ import { HmacSHA256 } from "crypto-js";
 export async function POST(request: NextRequest) {
    const { username } = await request.json();
       var clientId = process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID || ""
-      var clientSecret = process.env.NEXT_KEY_COGNITO_ID || ""
+      var clientSecret = process.env.NEXT_PUBLIC_KEY_COGNITO_ID || ""
       const secretHash = HmacSHA256(username + clientId, clientSecret).toString(Base64)
 
    const command = new ForgotPasswordCommand({
